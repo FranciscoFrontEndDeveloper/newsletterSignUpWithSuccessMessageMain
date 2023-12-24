7b059631623b0637c822b92ff512e2c6fbaf35d5
FRANCISCO <pachonetworking@gmail.com>
2023-12-24 18:37:56 -0500

    desktop image config

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -5,8 +5,22 @@ import { CommonModule } from '@angular/common';
   selector: 'app-image',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './image.component.html'
+  templateUrl: './image.component.html',
 })
 export class ImageComponent {
+  public imageForm!: string;
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.changeImage();
+  }
+
+  changeImage() {
+    if (window.innerWidth > 1024) {
+      this.imageForm = 'assets/images/illustration-sign-up-desktop.svg';
+    } else {
+      this.imageForm = 'assets/images/illustration-sign-up-mobile.svg';
+    }
+  }
 }
