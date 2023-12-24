bff184b6fcc1cd18df31516769b6cc0ad1833147
FRANCISCO <pachonetworking@gmail.com>
2023-12-24 18:21:16 -0500

    div center succes

---
+++ src/app/components/thanks/thanks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-thanks',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './thanks.component.html'
+})
+export class ThanksComponent {
+
+}
