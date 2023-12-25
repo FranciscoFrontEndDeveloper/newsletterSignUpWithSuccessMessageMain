161141af403fef16f96fd6fa3e1d83bb1555f627
FRANCISCO <pachonetworking@gmail.com>
2023-12-24 21:18:05 -0500

    thanks component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { ThanksComponent } from './components/thanks/thanks.component';
 
 export const routes: Routes = [
- {path: '', component: MainComponent}
+ { path: '', component: MainComponent },
+ {path: 'thanks', component: ThanksComponent}
 ];
