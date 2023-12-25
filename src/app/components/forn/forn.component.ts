fc38670867ea6e421ef138592ab00b9d2cdc881f
FRANCISCO <pachonetworking@gmail.com>
2023-12-25 03:22:15 -0500

    form style success

--- src/app/components/forn/forn.component.ts
+++ src/app/components/forn/forn.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { routes } from '../../app.routes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forn',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './forn.component.html'
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './forn.component.html',
 })
 export class FornComponent {
+  public newsForm!: FormGroup;
+  public errorForm!: boolean;
+  constructor(private FormBuilder: FormBuilder, private Router: Router) {
+    this.newsForm = this.FormBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+    this.errorForm = false
+  }
 
+  onSubmit() {
+    if (this.newsForm.valid) {
+      this.Router.navigate(['/', 'thanks']);
+    } else {
+      this.Router.navigate(['/', '']);
+      this.errorForm = true;
+    }
+  }
 }
