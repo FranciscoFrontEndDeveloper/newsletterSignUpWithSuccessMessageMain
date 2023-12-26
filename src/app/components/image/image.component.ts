9904f96245cc7be576ac39f00687db65ebd37051
FRANCISCO <pachonetworking@gmail.com>
2023-12-25 19:22:03 -0500

    challenge finished

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,17 +10,22 @@ import { CommonModule } from '@angular/common';
 export class ImageComponent {
   public imageForm!: string;
 
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.changeImage();
-  }
-
-  changeImage() {
+  // @HostListener('window:click')
+  // onclick() {
+  //   console.log('click');
+  // }
+  @HostListener('window:resize')
+  onResize() {
     if (window.innerWidth > 1024) {
       this.imageForm = 'assets/images/illustration-sign-up-desktop.svg';
     } else {
       this.imageForm = 'assets/images/illustration-sign-up-mobile.svg';
     }
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    // this.changeImage();
+    this.onResize();
   }
 }
