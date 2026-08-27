93e4484eade8e6e0e57ebb215b013ad40bcb8b81
Francisco Becerra Frontend Angular Developer <40615428+FranciscoFrontEndDeveloper@users.noreply.github.com>
2026-08-27 16:40:13 -0500

    fixer item image

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,18 +7,4 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './image.component.html',
 })
-export class ImageComponent implements OnInit {
-  public imageSrc!: string;
-
-  @HostListener('window:resize')
-  onResize() {
-    this.imageSrc = window.innerWidth > 1024
-      ? 'assets/images/illustration-sign-up-desktop.svg'
-      : 'assets/images/illustration-sign-up-mobile.svg';
-  }
-
-  ngOnInit(): void {
-
-    this.onResize();
-  }
-}
+export class ImageComponent {}
