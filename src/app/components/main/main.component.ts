02156952556fa720919a7bfae24c329690746e7c
Francisco Becerra Frontend Angular Developer <40615428+FranciscoFrontEndDeveloper@users.noreply.github.com>
2026-08-27 16:21:17 -0500

    correction errors 11 to 13

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { ImageComponent } from "../image/image.component";
 import { FornComponent } from "../forn/forn.component";
 
@@ -7,6 +7,6 @@ import { FornComponent } from "../forn/forn.component";
     selector: 'app-main',
     standalone: true,
     templateUrl: './main.component.html',
-    imports: [CommonModule, ImageComponent, FornComponent]
+    imports: [ ImageComponent, FornComponent]
 })
 export class MainComponent {}
